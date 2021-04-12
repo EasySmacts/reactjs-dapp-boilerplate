20c82decc92f38f84c94893592a84edc0cdc2763
DavidCuentasMar <davidandrescuentasmar@gmail.com>
2021-04-12 07:02:01 -0500

    MetaMask Integration & Truffle Structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3'
 
 function App() {
+  const [web3State, setWeb3State] = useState(false);
+
+  const loadWeb3 = async()=> {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+      setWeb3State(true)
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+      setWeb3State(true)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  useEffect(() => {
+    if (!web3State) {
+      loadWeb3();
+    }
+  }, [web3State]);
+
   return (
     <div className="App">
       <header className="App-header">
